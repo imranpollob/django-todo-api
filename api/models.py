dd1b4e7057c6b739b77c80e780b7f85f9775cb44
pollmix <polboy777@gmail.com>
2021-06-17 18:26:57 +0600

    todo + admin panel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=255)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} {self.is_completed}"
